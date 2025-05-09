93711ba19fab98b6e6df1a059e44ac700ecafae6
benjaminburgo <bburgo@mail.austral.edu.ar>
2025-05-08 23:30:12 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -18,13 +18,10 @@ def check_lists(lst1, lst2):
 
 def list_of_lists(list_of_lists_to_modify):
     return [
-        list_of_lists_to_modify[0][:2] if len(list_of_lists_to_modify[0]) >= 2 else [],
-        list_of_lists_to_modify[1][1:4] if len(list_of_lists_to_modify[1]) >= 2 else [],
-        list_of_lists_to_modify[2][-2:] if len(list_of_lists_to_modify[2]) >= 2 else []
+        list_of_lists_to_modify[0][:2],
+        list_of_lists_to_modify[1][1:4],
+        list_of_lists_to_modify[2][-2:]
     ]
-
-
-
 
 
 
