b407d69754a62299aee819ea0076ea912a0d5f8b
benjaminburgo <bburgo@mail.austral.edu.ar>
2025-05-08 23:27:23 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,40 +1,28 @@
 def remove_elements(lst):
-    if 'Red' in lst:
-        lst.remove('Red')
-    if 'Pink' in lst:
-        lst.remove('Pink')
-    if 'Yellow' in lst:
-        lst.remove('Yellow')
-    return lst
+    lst_copy = lst[:]
+    for i in sorted([0, 4, 5], reverse=True):
+        if i < len(lst_copy):
+            del lst_copy[i]
+    return lst_copy
 
 def add_elements(lst):
-    lst.insert(0, 'Pink')
-    lst.append('Yellow')
-    return lst
+    return ['Pink'] + lst + ['Yellow']
 
 def is_empty(lst):
     return len(lst) == 0
 
 def check_lists(lst1, lst2):
-    return sorted(lst1) == sorted(lst2)
+    if len(lst1) > 2 and len(lst2) > 2:
+        return lst1[2] == lst2[2]
+    return False
 
 def list_of_lists(list_of_lists_to_modify):
-    if len(list_of_lists_to_modify[0]) >= 2:
-        list_of_lists_to_modify[0] = list_of_lists_to_modify[0][:2]
-    else:
-        list_of_lists_to_modify[0] = []
+    return [
+        list_of_lists_to_modify[0][:2] if len(list_of_lists_to_modify[0]) >= 2 else [],
+        list_of_lists_to_modify[1][1:4] if len(list_of_lists_to_modify[1]) >= 2 else [],
+        list_of_lists_to_modify[2][-2:] if len(list_of_lists_to_modify[2]) >= 2 else []
+    ]
 
-    if len(list_of_lists_to_modify[1]) >= 4:
-        list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
-    else:
-        list_of_lists_to_modify[1] = []
-
-    if len(list_of_lists_to_modify[2]) >= 2:
-        list_of_lists_to_modify[2] = list_of_lists_to_modify[2][-2:]
-    else:
-        list_of_lists_to_modify[2] = []
-
-    return list_of_lists_to_modify
 
 
 
